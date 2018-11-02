c31898af76ead7b123833744d3617c04adfe601b
Elly <elly@mg.crane.ai>
2018-11-01 23:03:35 -0400

    added client/src/components/composite refactoring

---
+++ client/src/components/composite.jsx
@@ -0,0 +1,83 @@
+
+/*
+composite view
+created by: FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+the composite view has a status bar located at the very-top across the full width of the view
+composite view has an Image located at the very-top across the full width of the view. this is a placeholder and should be updated with higher resolution Image
+composite view has a Label located at the main area left of the view with text axe/me strea
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col   } from 'reactstrap';
+        
+        
+        import {  }  from '../actions/compositeActions.js'; 
+
+        
+        
+       
+        class compositeView extends React.Component {
+
+            state = {
+                
+                
+            }
+    
+                
+            
+
+            componentDidMount = () => {
+                
+               
+                
+            }
+    
+        render() {
+        
+        
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                 
+                    <Col md={12}>
+
+                        			<Col md={12} className="text-center">				{/* <!-- TODO: Change placeholder image with higher res --> */ }
+				<img className="img-fluid" alt="Responsive image" src='http://path.to/replace/image' /> 
+
+			</Col>			<Col md={12}>
+				<p className="label-block-very-light-yellow-paragraph"> axe/me strea </p>
+			</Col>
+
+
+                           
+   
+                    </Col> 
+                     
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({   }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(compositeView);
+    
+        

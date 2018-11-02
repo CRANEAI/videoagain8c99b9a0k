5a16525cbabdf3a86fbeac85f7bb22eb8d2df6ba
Elly <elly@mg.crane.ai>
2018-11-01 22:53:42 -0400

    added client/src/actions/ refactoring

---
+++ client/src/actions/.iii_sketch_‘3‘Actions.js
@@ -0,0 +1,5 @@
+/*
+                This will control the networking for the .iii sketch ‘3‘ network requests
+            */
+import axios from "axios"
+import { client, url, setToken } from "../utils/axios.config.js"

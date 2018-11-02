e61356e1846aedc29c011612b115aa342fc7ab37
Elly <elly@mg.crane.ai>
2018-11-02 02:34:12 -0400

    updated client/src/components/ refactoring

--- client/src/components/.iii_sketch_‘3‘.jsx
+++ client/src/components/.iii_sketch_‘3‘.jsx
@@ -7,32 +7,30 @@ created by: FusionCode
 // Component Requirements
 //----------------------------------------------------------------------------------------------------------
 
-the follow view is a data-bound list based view with a repeating template containing User Account Photo, Table Row elements
-the follow view has a status bar located at the very-top across the full width of the view
-follow view has Navigation Bar located at the very-top in the center of the view with text .iii sketch ‘3‘
-follow view has Label located at the very-top in the center of the view, text needs identified
-follow view has Label located at the top center of the view, text needs identified
-follow view has Table View that scrolls horizontal
-follow view has Label located at the top center of the view, text needs identified
-follow view has Label located at the top center of the view, text needs identified
-follow view has Label located at the top center of the view, text needs identified
-follow view has Label located at the mid-top center of the view, text needs identified
-follow view has Table View that scrolls horizontal
-follow view has Label located at the mid-top center of the view, text needs identified
-follow view has Label located at the mid-top center of the view, text needs identified
-follow view has Table View that scrolls vertical
-follow view has Label located at the main area center of the view, text needs identified
-follow view has a Button located at the main area center of the view with text follow
-follow view has Label located at the main area center of the view, text needs identified
-follow view has an Image located at the mid-bottom left of the view. this is a placeholder and should be updated with higher resolution Image
-follow view has Label located at the mid-bottom center of the view, text needs identified
-follow view has Label located at the mid-bottom center of the view, text needs identified
-follow view has Label located at the mid-bottom center of the view, text needs identified
-follow view has Label located at the mid-bottom center of the view, text needs identified
-follow view has Label located at the bottom center of the view, text needs identified
-follow view has Label located at the bottom center of the view, text needs identified
-follow view has Label located at the bottom center of the view, text needs identified
-follow view has a Button located at the bottom center of the view with text follow
+the feed view is a data-bound list based view with a repeating template containing User Account Photo, Table Row elements
+the feed view has a status bar located at the very-top across the full width of the view
+feed view has Navigation Bar located at the very-top across the full width of the view
+feed view has Label located at the very-top in the center of the view, text needs identified
+feed view has Label located at the top center of the view, text needs identified
+feed view has Table View that scrolls horizontal
+feed view has a Button located at the top center of the view with text follow
+feed view has Label located at the top center of the view, text needs identified
+feed view has Label located at the top center of the view, text needs identified
+feed view has Label located at the mid-top center of the view, text needs identified
+feed view has Table View that scrolls horizontal
+feed view has Label located at the mid-top center of the view, text needs identified
+feed view has a Button located at the mid-top center of the view with text follow
+feed view has Table View that scrolls vertical
+feed view has Label located at the main area center of the view, text needs identified
+feed view has Label located at the main area center of the view, text needs identified
+feed view has an Image located at the mid-bottom left of the view. this is a placeholder and should be updated with higher resolution Image
+feed view has Label located at the mid-bottom center of the view, text needs identified
+feed view has Label located at the mid-bottom center of the view, text needs identified
+feed view has Label located at the mid-bottom center of the view, text needs identified
+feed view has Label located at the bottom center of the view, text needs identified
+feed view has Label located at the bottom center of the view, text needs identified
+feed view has Label located at the bottom center of the view, text needs identified
+feed view has a Button located at the bottom center of the view with text follow
 view has actions which trigger on events, view should should error messages and display activity indicator when processing
 
 */
@@ -40,28 +38,36 @@ view has actions which trigger on events, view should should error messages and 
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Row, Col   } from 'reactstrap';
+import { Row, Col , Button, Input, InputGroup, InputGroupText } from 'reactstrap';
         
         
         import {  }  from '../actions/.iii_sketch_‘3‘Actions.js'; 
 
-        import { get_live_data, get_.iii_sketch_‘3‘_data, get_follow_data }  from '../actions/.iii sketch ‘3‘Actions.js'; 
+        import { get_sign_up_data, get_.iii_sketch_‘3‘_data, get_.iii_sketch_‘3‘_data, get_feed_data, get_live_data }  from '../actions/.iii sketch ‘3‘Actions.js'; 
         
        
         class .iiisketch‘3‘View extends React.Component {
 
             state = {
                 
-                
+                name : '', 
+email : '', 
+password : '', 
+
             }
     
-                
             
-        get_live_data = () => {
+        handleChange(e) {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }    
+            
+        get_sign up_data = () => {
 
             return (
                 <Col>
-                <!-- ToDo: Refine template loop for get_live_data -->
+                <!-- ToDo: Refine template loop for get_sign up_data -->
                 </Col>
             )
         }
@@ -75,20 +81,38 @@ import { Row, Col   } from 'reactstrap';
             )
         }
         
-        get_follow_data = () => {
+        get_.iii sketch ‘3‘_data = () => {
 
             return (
                 <Col>
-                <!-- ToDo: Refine template loop for get_follow_data -->
+                <!-- ToDo: Refine template loop for get_.iii sketch ‘3‘_data -->
+                </Col>
+            )
+        }
+        
+        get_feed_data = () => {
+
+            return (
+                <Col>
+                <!-- ToDo: Refine template loop for get_feed_data -->
+                </Col>
+            )
+        }
+        
+        get_live_data = () => {
+
+            return (
+                <Col>
+                <!-- ToDo: Refine template loop for get_live_data -->
                 </Col>
             )
         }
         
 
             componentDidMount = () => {
-                const { get_live_data, get_.iii_sketch_‘3‘_data, get_follow_data } = this.props
+                const { get_sign_up_data, get_.iii_sketch_‘3‘_data, get_.iii_sketch_‘3‘_data, get_feed_data, get_live_data } = this.props
                
-                ,,
+                ,,,,
             }
     
         render() {
@@ -102,10 +126,56 @@ import { Row, Col   } from 'reactstrap';
                  
                     <Col md={12}>
 
-                        
-                        {render_get_live_data()}
+                        			<Col md={12}>
+				{/* <!-- TODO: Create unit test for sign_up button-white-standard-medium action --> */}
+				<Button 
+                        className="btn btn-lg btn-block button-white-standard-medium" 
+                        onClick={() => this.sign_up()}>sign up</Button> 
+
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-gray-white" 
+                                        value={this.state.name}
+                                        onChange={this.handleChange}
+                                        name="name"
+                                        placeholder="name" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-white-standard-small"> ryan hickman </p>
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-gray-white" 
+                                        value={this.state.email}
+                                        onChange={this.handleChange}
+                                        name="email"
+                                        placeholder="email" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-white-standard-small"> ryan@ryan‘com </p>
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-white-gray" 
+                                        value={this.state.password}
+                                        onChange={this.handleChange}
+                                        name="password"
+                                        placeholder="password" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-light-turquoise-light-sky-blue"> ism—in </p>
+			</Col>
+
+
+                        {render_get_sign up_data()}
 {render_get_.iii_sketch_‘3‘_data()}
-{render_get_follow_data()}
+{render_get_.iii sketch ‘3‘_data()}
+{render_get_feed_data()}
+{render_get_live_data()}
    
    
                     </Col> 
@@ -121,7 +191,7 @@ import { Row, Col   } from 'reactstrap';
         }
     
         function mapDispatchToProps(dispatch) {
-            return bindActionCreators({  get_live_data, get_.iii_sketch_‘3‘_data, get_follow_data }, dispatch);
+            return bindActionCreators({  get_sign_up_data, get_.iii_sketch_‘3‘_data, get_.iii_sketch_‘3‘_data, get_feed_data, get_live_data }, dispatch);
         }
     
    

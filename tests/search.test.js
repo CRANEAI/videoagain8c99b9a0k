be8f067e7634e661ed7063aa8d58699472d2643a
Elly <elly@mg.crane.ai>
2018-11-01 23:59:23 -0400

    added tests/search updated project details in new api

---
+++ tests/search.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - search UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action */
+it("the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

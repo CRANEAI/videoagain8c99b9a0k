4ac07fb55aa97a2934490ec30219014ecd936cc6
Elly <elly@mg.crane.ai>
2018-11-02 00:11:04 -0400

    added tests/chat updated project details in new api

---
+++ tests/chat.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - chat UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* chat view has a Label located at the bottom center of the view with text s */
+it("chat view has a Label located at the bottom center of the view with text s", () => {
+  /* ToDo developer needs to complete unit test 
+               chat view has a Label located at the bottom center of the view with text s
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

fac9a93973cc9c69676564372d5afcd8aeaa0ac9
Elly <elly@mg.crane.ai>
2018-11-01 23:03:41 -0400

    added tests/composite refactoring

---
+++ tests/composite.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - composite UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* composite view has a Label located at the main area left of the view with text axe/me strea */
+it("composite view has a Label located at the main area left of the view with text axe/me strea", () => {
+  /* ToDo developer needs to complete unit test 
+               composite view has a Label located at the main area left of the view with text axe/me strea
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

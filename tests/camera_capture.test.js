770bd86e097829d7df929d119ca55037c14afa74
Elly <elly@mg.crane.ai>
2018-11-01 23:01:00 -0400

    added tests/camera_capture updated project details in new api

---
+++ tests/camera_capture.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - camera capture UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* camera capture view has a Label located at the main area left of the view with text axe/me strea */
+it("camera capture view has a Label located at the main area left of the view with text axe/me strea", () => {
+  /* ToDo developer needs to complete unit test 
+               camera capture view has a Label located at the main area left of the view with text axe/me strea
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

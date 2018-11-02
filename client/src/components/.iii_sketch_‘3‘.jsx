f1cef38cd98e72343f1b3760861aa28d060e45ec
Elly <elly@mg.crane.ai>
2018-11-01 22:53:12 -0400

    added client/src/components/ refactoring

---
+++ client/src/components/.iii_sketch_‘3‘.jsx
@@ -0,0 +1,172 @@
+
+/*
+.iii sketch ‘3‘ view
+created by: FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+the notifications view is a data-bound list based view with a repeating template containing User Account Photo, Table Row elements
+the notifications view has a status bar located at the very-top across the full width of the view
+notifications view has Navigation Bar located at the very-top in the center of the view with text .iii sketch ‘3‘
+notifications view has a Datatype located at the very-top in the center of the view with text notifications when they are live
+notifications view has Label located at the top center of the view, text needs identified
+notifications view has Table View that scrolls horizontal
+notifications view has Label located at the top center of the view, text needs identified
+notifications view has Label located at the top center of the view, text needs identified
+notifications view has Label located at the top center of the view, text needs identified
+notifications view has Label located at the mid-top center of the view, text needs identified
+notifications view has Table View that scrolls horizontal
+notifications view has a Label located at the mid-top center of the view with text id: 34983532
+notifications view has a Label located at the mid-top center of the view with text & ld: 64983274
+notifications view has Table View that scrolls vertical
+notifications view has Label located at the main area center of the view, text needs identified
+notifications view has a Button located at the main area center of the view with text follow
+notifications view has Label located at the main area center of the view, text needs identified
+notifications view has an Image located at the mid-bottom left of the view. this is a placeholder and should be updated with higher resolution Image
+notifications view has Label located at the mid-bottom center of the view, text needs identified
+notifications view has Label located at the mid-bottom center of the view, text needs identified
+notifications view has Label located at the mid-bottom center of the view, text needs identified
+notifications view has Label located at the mid-bottom center of the view, text needs identified
+notifications view has Label located at the bottom center of the view, text needs identified
+notifications view has Label located at the bottom center of the view, text needs identified
+notifications view has a Label located at the bottom center of the view with text ‘_ id: 63283294
+notifications view has a Button located at the bottom center of the view with text follow
+view has actions which trigger on events, view should should error messages and display activity indicator when processing
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col , Button, Input, InputGroup, InputGroupText } from 'reactstrap';
+        
+        
+        import {  }  from '../actions/.iii_sketch_‘3‘Actions.js'; 
+
+        import { get_follow_data, get_.iii_sketch_‘3‘_data }  from '../actions/.iii sketch ‘3‘Actions.js'; 
+        
+       
+        class .iiisketch‘3‘View extends React.Component {
+
+            state = {
+                
+                name : '', 
+email : '', 
+password : '', 
+
+            }
+    
+            
+        handleChange(e) {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }    
+            
+        get_follow_data = () => {
+
+            return (
+                <Col>
+                <!-- ToDo: Refine template loop for get_follow_data -->
+                </Col>
+            )
+        }
+        
+        get_.iii_sketch_‘3‘_data = () => {
+
+            return (
+                <Col>
+                <!-- ToDo: Refine template loop for get_.iii_sketch_‘3‘_data -->
+                </Col>
+            )
+        }
+        
+
+            componentDidMount = () => {
+                const { get_follow_data, get_.iii_sketch_‘3‘_data } = this.props
+               
+                ,
+            }
+    
+        render() {
+        
+        
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                 
+                    <Col md={12}>
+
+                        			<Col md={12}>
+				{/* <!-- TODO: Create unit test for sign_up button-white-standard-medium action --> */}
+				<Button 
+                        className="btn btn-lg btn-block button-white-standard-medium" 
+                        onClick={() => this.sign_up()}>sign up</Button> 
+
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-gray-white" 
+                                        value={this.state.name}
+                                        onChange={this.handleChange}
+                                        name="name"
+                                        placeholder="name" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-white-standard-small"> ryan hickman </p>
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-gray-white" 
+                                        value={this.state.email}
+                                        onChange={this.handleChange}
+                                        name="email"
+                                        placeholder="email" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-white-standard-small"> ryan@ryan‘com </p>
+			</Col>
+
+			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-white-gray" 
+                                        value={this.state.password}
+                                        onChange={this.handleChange}
+                                        name="password"
+                                        placeholder="password" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-light-turquoise-light-sky-blue"> ism—in </p>
+			</Col>
+
+
+                        {render_get_follow_data()}
+{render_get_.iii_sketch_‘3‘_data()}
+   
+   
+                    </Col> 
+                     
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({  get_follow_data, get_.iii_sketch_‘3‘_data }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(.iiisketch‘3‘View);
+    
+        

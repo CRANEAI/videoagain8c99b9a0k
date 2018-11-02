283062ed8e0fa035c4e5f915415ccb6882714bc7
Elly <elly@mg.crane.ai>
2018-11-01 23:00:01 -0400

    added client/src/actions/followersActions updated project details in new api

---
+++ client/src/actions/followersActions.js
@@ -0,0 +1,5 @@
+/*
+                This will control the networking for the followers network requests
+            */
+import axios from "axios"
+import { client, url, setToken } from "../utils/axios.config.js"

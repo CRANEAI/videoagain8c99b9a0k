de8e3081e0eb43aec2244928a44fb90e7368faed
Elly <elly@mg.crane.ai>
2018-11-01 23:58:09 -0400

    added tests/get_started updated project details in new api

---
+++ tests/get_started.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - get started UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* the get started view has a status bar located at the very-top across the full width of the view */
+it("the get started view has a status bar located at the very-top across the full width of the view", () => {
+  /* ToDo developer needs to complete unit test 
+               the get started view has a status bar located at the very-top across the full width of the view
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

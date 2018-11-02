1a9ddd8d27d0a32ea015a7ef115a45ac2c4cfded
Elly <elly@mg.crane.ai>
2018-11-01 22:53:17 -0400

    added server/api/v1/routes/notifications refactoring

---
+++ server/api/v1/routes/notifications.js
@@ -0,0 +1,127 @@
+const router = require("express").Router()
+const passport = require("../auth")
+const { db } = require("../../../utils")
+
+//----------------------------------------------------------------------------------------------------------
+//Endpoints
+//----------------------------------------------------------------------------------------------------------
+
+/**
+ * @swagger
+ * api/v1/notifications/follow:
+ *   post:
+ *     tags:
+ *       - name: follow
+ *     description: follow
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: it works!
+ */
+router.post(
+  "/follow",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    follow().then(result => {
+      res.status(200).send(result)
+    })
+  }
+)
+
+/**
+ * @swagger
+ * api/v1/notifications/follow:
+ *   post:
+ *     tags:
+ *       - name: follow
+ *     description: follow
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: it works!
+ */
+router.post(
+  "/follow",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    follow().then(result => {
+      res.status(200).send(result)
+    })
+  }
+)
+
+/**
+ * @swagger
+ * api/v1/notifications/follow:
+ *   post:
+ *     tags:
+ *       - name: follow
+ *     description: follow
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: it works!
+ */
+router.post(
+  "/follow",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    follow().then(result => {
+      res.status(200).send(result)
+    })
+  }
+)
+
+/**
+ * @swagger
+ * follow():
+ *
+ */
+follow = () => {
+  return new Promise((resolve, reject) => {
+    /* TODO - write follow function */
+    resolve()
+
+    /* TODO - handle follow error
+                        reject({message: "error"})
+                    */
+  })
+}
+
+/**
+ * @swagger
+ * follow():
+ *
+ */
+follow = () => {
+  return new Promise((resolve, reject) => {
+    /* TODO - write follow function */
+    resolve()
+
+    /* TODO - handle follow error
+                        reject({message: "error"})
+                    */
+  })
+}
+
+/**
+ * @swagger
+ * follow():
+ *
+ */
+follow = () => {
+  return new Promise((resolve, reject) => {
+    /* TODO - write follow function */
+    resolve()
+
+    /* TODO - handle follow error
+                        reject({message: "error"})
+                    */
+  })
+}

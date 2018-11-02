390112ef610cda890af509475bde8285bc7167b4
Elly <elly@mg.crane.ai>
2018-11-01 23:20:05 -0400

    added tests/edit_profile refactoring

---
+++ tests/edit_profile.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - edit profile UNIT TESTS
+ * Application Client
+ * Video Again
+*/
+
+/* the edit profile view has a home icon located at the bottom left of the view which on click will trigger the home() action */
+it("the edit profile view has a home icon located at the bottom left of the view which on click will trigger the home() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the edit profile view has a home icon located at the bottom left of the view which on click will trigger the home() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

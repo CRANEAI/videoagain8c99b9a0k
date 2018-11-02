b91933404792c857d8f10610c84d2c13418c419e
Elly <elly@mg.crane.ai>
2018-11-02 01:11:02 -0400

    updated server updated express server

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const express = require("express")
-const path = require("path")
+
+            const express = require("express"); 
+const path = require("path"); 
 const cors = require("cors")
 
+
+console.log("
+
+
+
+
+
+
+
+
+
+
+")
 console.log(`
 Created with
 ███████╗██╗   ██╗███████╗██╗ ██████╗ ███╗   ██╗         ██████╗ ██████╗ ██████╗ ███████╗
@@ -15,54 +29,50 @@ Fusion_Code. 2018.
 * 
 *`)
 
-global.__basedir = __dirname
+            global.__basedir = __dirname;
 
-const app = express()
-app.use(
-  express.json({
-    extended: false
-  })
-)
+            const app = express()
+            app.use(express.json({
+                extended: false
+              }))
+            
+            app.use(cors())
+            
+            app.disable("x-powered-by");
 
-app.use(cors())
+            app.use((req, res, next) => {
+                res.setHeader("X-Content-Type-Options", "nosniff");
+                res.setHeader("X-XSS-Protection", "1");
+                res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+                next()
+              })
+            
+            /**
+            * Api Directories
+            * 
+            */
+            app.use("/api/v1", require("./server/api/v1/routes"))
 
-app.disable("x-powered-by")
+            /**
+             * Static file serving directories
+             * 
+             */
+            app.use(express.static(path.join(__dirname, 'public/')));
+            app.use(express.static(path.join(__dirname, 'client/')));
+            app.use(express.static(path.join(__dirname, 'node_modules')));
+         
+            app.get('*', function (req, res){
+                res.sendFile(path.join(__dirname, 'client', 'index.html'))
+              })
 
-app.use((req, res, next) => {
-  res.setHeader("X-Content-Type-Options", "nosniff")
-  res.setHeader("X-XSS-Protection", "1")
-  res.setHeader(
-    "Strict-Transport-Security",
-    "max-age=31536000; includeSubDomains"
-  )
-  next()
-})
+            /**
+             * Launch Server
+             * 
+             */
 
-/**
- * Api Directories
- *
- */
-app.use("/api/v1", require("./server/api/v1/routes"))
+            const port = process.env.PORT || 5000;
 
-/**
- * Static file serving directories
- *
- */
-app.use(express.static(path.join(__dirname, "public/")))
-app.use(express.static(path.join(__dirname, "client/")))
-app.use(express.static(path.join(__dirname, "node_modules")))
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "client", "index.html"))
-})
-
-/**
- * Launch Server
- *
- */
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log("listening on port", port)
-})
+            app.listen(port, ()=>{
+                console.log("listening on port", port)
+            })
+            

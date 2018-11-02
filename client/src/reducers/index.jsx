b309e032f86909c594a81241afafef4f9863f79a
Elly <elly@mg.crane.ai>
2018-11-01 23:14:10 -0400

    updated client/src/reducers/index refactoring

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -2,16 +2,20 @@
         import { combineReducers }                      from 'redux';
         import { reducer as Form }                      from 'redux-form';
 
-        import notifications from './reducer_notifications' 
-import .iii_sketch_‘3‘ from './reducer_.iii_sketch_‘3‘' 
+        import .iii_sketch_‘3‘ from './reducer_.iii_sketch_‘3‘' 
 import follow from './reducer_follow' 
-import composite from './reducer_composite'
+import composite from './reducer_composite' 
+import live from './reducer_live' 
+import .iii_sketch_‘3‘ from './reducer_.iii_sketch_‘3‘' 
+import notifications from './reducer_notifications'
 
         const rootReducer = combineReducers({
-            notifications : notifications 
-,.iii_sketch_‘3‘ : .iii_sketch_‘3‘ 
+            .iii_sketch_‘3‘ : .iii_sketch_‘3‘ 
 ,follow : follow 
 ,composite : composite 
+,live : live 
+,.iii_sketch_‘3‘ : .iii_sketch_‘3‘ 
+,notifications : notifications 
 
         })
 

9d751adea94ddc60ec05f8890023de998cede246
Elly <elly@mg.crane.ai>
2018-11-01 23:59:36 -0400

    added server/api/v1/routes/search updated project details in new api

---
+++ server/api/v1/routes/search.js
@@ -0,0 +1,504 @@
+
+                const router = require("express").Router();
+                const passport = require("../auth");
+                const {db} = require("../../../utils");
+
+                //----------------------------------------------------------------------------------------------------------
+                //Endpoints
+                //----------------------------------------------------------------------------------------------------------
+    
+                
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close,:
+                *   post:
+                *     tags:
+                *       - name: close,
+                *     description: close,
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close,',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close,( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+
+               /**
+                * @swagger
+                * api/v1/search/close:
+                *   post:
+                *     tags:
+                *       - name: close
+                *     description: close
+                *     produces:
+                *       - application/json
+                *     parameters:
+                *     responses:
+                *       200:
+                *         description: it works!
+                */
+               router.post('/close',
+                   passport.authenticate("jwt", {session:false}), 
+                   (req, res) => {
+                        close( ).then((result) => {
+                            res.status(200).send(result)
+                        })                    
+               })
+               
+               
+               
+                
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close,():
+                *   
+                */
+               close, = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close, function */
+                    resolve()
+
+                    /* TODO - handle close, error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+               /**
+                * @swagger
+                * close():
+                *   
+                */
+               close = () => {
+               return new Promise((resolve, reject) => {
+                    /* TODO - write close function */
+                    resolve()
+
+                    /* TODO - handle close error
+                        reject({message: "error"})
+                    */
+                })
+               }
+           
+                

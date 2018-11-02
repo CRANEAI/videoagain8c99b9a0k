eb49c373db221d3f3a428dd66c398dd608b7ba63
Elly <elly@mg.crane.ai>
2018-11-01 23:53:12 -0400

    updated client/src/reducers/index updated project details in new api

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -2,20 +2,26 @@
         import { combineReducers }                      from 'redux';
         import { reducer as Form }                      from 'redux-form';
 
-        import .iii_sketch_‘3‘ from './reducer_.iii_sketch_‘3‘' 
+        import get_started from './reducer_get_started' 
+import feed from './reducer_feed' 
+import .iii_sketch_‘3‘ from './reducer_.iii_sketch_‘3‘' 
 import live from './reducer_live' 
 import follow from './reducer_follow' 
-import follow from './reducer_follow' 
 import edit_profile from './reducer_edit_profile' 
-import follow from './reducer_follow'
+import composite from './reducer_composite' 
+import search from './reducer_search' 
+import notifications from './reducer_notifications'
 
         const rootReducer = combineReducers({
-            .iii_sketch_‘3‘ : .iii_sketch_‘3‘ 
+            get_started : get_started 
+,feed : feed 
+,.iii_sketch_‘3‘ : .iii_sketch_‘3‘ 
 ,live : live 
 ,follow : follow 
-,follow : follow 
 ,edit_profile : edit_profile 
-,follow : follow 
+,composite : composite 
+,search : search 
+,notifications : notifications 
 
         })
 
